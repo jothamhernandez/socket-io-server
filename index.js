fd0b46a6944aefece03efe1cef0c1240df860422
vagrant <vagrant@homestead>
2019-06-07 19:42:20 +0000

    modified index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-
 app.listen(8080);
+
+var users = [];
 
 function handler(req, res){
 	fs.readFile(__dirname + '/index.html',
@@ -18,9 +19,23 @@ function handler(req, res){
 }
 
 io.on('connection', function(socket){
-	socket.emit('news', {hello:'world'});
-
-	socket.on('my other event', function(data){
-		console.log(data);
+	var username;
+	socket.emit('connected', {connected: users.length + 1});
+	socket.on('connected', function(data){
+		username = data.data;
+		users.push(username);
+		socket.emit('link start', {message:"you are connected",connected: users.length});
 	});
+
+
+	socket.broadcast.emit('new connection', {message:"new user connected", connected:users.length + 1});
+
+	socket.on('disconnect', function(){
+		users.splice(users.indexOf(username),1);
+		setTimeout(function(){
+
+			socket.broadcast.emit('user disconnect', {message:"disconnected", connected: users.length});
+		}, 2000);
+	});
+
 });
